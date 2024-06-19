4ca923e369077396e0c2cd9da4cc5591909d095c
Geraldo Luiz <geraldobl58@gmail.com>
2024-06-19 12:22:06 -0300

    feat: Add clerk auth and middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
 const font = Inter({ subsets: ["latin"] });
@@ -17,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
+
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+import { Loader } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
 export const Header = () => {
   return (
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-      <div>Header</div>
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="flex items-center gap-x-3 pt-8 pl-4 pb-7">
+          <Image alt="Mascot" width={40} height={40} src="/mascot.svg" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="size-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              mode="modal"
+              afterSignInUrl="/learn"
+              afterSignUpUrl="/learn"
+            >
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </header>
   );
 };
